3941810b47f11ffd4234f8c749c06973edfe3c19
kgeronim <kevin.geronimo@outlook.com>
2020-05-25 00:09:16 +0000

    refactored working logic for maintainability

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,11 @@
 use std::iter::Peekable;
+use std::str::CharIndices;
 
 fn main() {
     let lang = String::from("data=\"stRing藏\";data2=1;data3=.12939+546.23;empty=\"\"");
-    let mut chars = lang.char_indices().peekable();
+    let mut tokens = Tokens::from(&lang);
 
-    while let Some((start_index, c)) = chars.next() {
-        let token = match c {
-            '"' => scan_string(&lang, &mut chars, start_index),
-            '+' | '-' | '*' | '/' | '=' => Token::Operator(c),
-            '(' | ')' | '{' | '}' | ';' => Token::Symbol(c),
-            'a'..='z' => scan_identifier(&lang, &mut chars, start_index),
-            '0'..='9' => scan_number(&lang, &mut chars, start_index),
-            _ => Token::Other(c),
-        };
-
+    while let Some(token) = tokens.next() {
         println!("{:?}", token)
     }
 }
@@ -28,50 +20,71 @@ enum Token<'a> {
     Symbol(char),
 }
 
-fn scan_string<'a, T: Iterator<Item = (usize, char)>>(
-    lang: &'a str,
-    chars: &mut T,
-    start_index: usize,
-) -> Token<'a> {
-    loop {
-        if let Some((end_index, c)) = chars.next() {
-            if c == '"' {
-                break Token::String(&lang[start_index + 1..end_index]);
+struct Tokens<'a> {
+    lang: &'a String,
+    chars: Peekable<CharIndices<'a>>,
+}
+
+impl<'a> Tokens<'a> {
+    fn from(lang: &'a String) -> Self {
+        Tokens {
+            lang,
+            chars: lang.char_indices().peekable(),
+        }
+    }
+
+    fn scan_string(&mut self, index_start: usize) -> Token<'a> {
+        loop {
+            if let Some((index_end, c)) = self.chars.next() {
+                if c == '"' {
+                    break Token::String(&self.lang[index_start + 1..index_end]);
+                }
+            } else {
+                panic!("unterminated string literal")
+            }
+        }
+    }
+
+    fn scan_number(&mut self, index_start: usize) -> Token<'a> {
+        loop {
+            if let Some((index_end, c)) = self.chars.peek() {
+                if c.is_digit(10) || *c == '.' {
+                    self.chars.next();
+                } else {
+                    break Token::Number(&self.lang[index_start..*index_end]);
+                }
+            }
+        }
+    }
+
+    fn scan_identifier(&mut self, index_start: usize) -> Token<'a> {
+        loop {
+            if let Some((index_end, c)) = self.chars.peek() {
+                if c.is_ascii_alphanumeric() {
+                    self.chars.next();
+                } else {
+                    break Token::Identifier(&self.lang[index_start..*index_end]);
+                }
+            }
+        }
+    }
+}
+
+impl<'a> Iterator for Tokens<'a> {
+    type Item = Token<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some((index_start, c)) = self.chars.next() {
+            match c {
+                '"' => Some(self.scan_string(index_start)),
+                '+' | '-' | '*' | '/' | '=' => Some(Token::Operator(c)),
+                '(' | ')' | '{' | '}' | ';' => Some(Token::Symbol(c)),
+                '0'..='9' => Some(self.scan_number(index_start)),
+                'a'..='z' => Some(self.scan_identifier(index_start)),
+                _ => Some(Token::Other(c)),
             }
         } else {
-            panic!("unterminated string literal")
-        }
-    }
-}
-
-fn scan_number<'a, T: Iterator<Item = (usize, char)>>(
-    lang: &'a str,
-    chars: &mut Peekable<T>,
-    start_index: usize,
-) -> Token<'a> {
-    loop {
-        if let Some((end_index, c)) = chars.peek() {
-            if c.is_digit(10) || *c == '.' {
-                chars.next();
-            } else {
-                break Token::Number(&lang[start_index..*end_index]);
-            }
-        }
-    }
-}
-
-fn scan_identifier<'a, T: Iterator<Item = (usize, char)>>(
-    lang: &'a str,
-    chars: &mut Peekable<T>,
-    start_index: usize,
-) -> Token<'a> {
-    loop {
-        if let Some((end_index, c)) = chars.peek() {
-            if c.is_ascii_alphanumeric() {
-                chars.next();
-            } else {
-                break Token::Identifier(&lang[start_index..*end_index]);
-            }
+            None
         }
     }
 }
